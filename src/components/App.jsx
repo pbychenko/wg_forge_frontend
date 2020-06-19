d2559aded20eda9f0239e4bfa2d241e9c068155f
pavel <pavelv87@gmail.com>
2020-06-19 14:20:26 +0600

    step 6 final finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -131,93 +131,6 @@ export default class App extends React.Component {
     
     return <Statistics data={statistics} />;
   }
-  
-  // sortOrders = () => {
-  //   const { sorting, orders } = this.state;
-    
-  //   if (sorting.total) {
-  //     const correctOrders = orders.map(order => ({ ...order, 'total': +order.total, 'created_at': +order.created_at }));
-  //     const sorted = _.sortBy(correctOrders, 'total');
-  //     const result = sorted.map(order => ({ ...order, 'total': String(order.total), 'created_at': String(order.created_at) }));
-  //     this.setState({ orders: result });
-  //   }
-
-  //   if (sorting.created_at) {
-  //     const correctOrders = orders.map(order => ({ ...order, 'total': +order.total, 'created_at': +order.created_at }));
-  //     const sorted = _.sortBy(correctOrders, 'created_at');
-  //     const result = sorted.map(order => ({ ...order, 'total': String(order.total), 'created_at': String(order.created_at) }));
-  //     this.setState({ orders: result });
-  //   }
-
-  //   if (sorting.transaction_id) {
-  //     const sorted = _.sortBy(orders, 'transaction_id');
-  //     // const result = sorted.map(order => ({ ...order, 'total': String(order.total), 'created_at': String(order.created_at) }));
-  //     this.setState({ orders: sorted });
-  //   }
-
-  //   if (sorting.card_type) {
-  //     const sorted = _.sortBy(orders, 'card_type');
-  //     // const result = sorted.map(order => ({ ...order, 'total': String(order.total), 'created_at': String(order.created_at) }));
-  //     this.setState({ orders: sorted });
-  //   }
-
-  //   if (sorting.location) {
-  //     const locationCompare = (a, b) => {
-  //       if (a.order_country < b.order_country) {
-  //         return -1;
-  //       }
-  //       if (a.order_country > b.order_country) {
-  //         return 1;
-  //       }
-
-  //       if (a.order_ip < b.order_ip) {
-  //         return -1;
-  //       }
-  //       if (a.order_ip > b.order_ip) {
-  //         return 1;
-  //       }
-
-  //       return 0;
-  //     }
-
-
-  //     const sorted = orders.sort(locationCompare);
-  //     this.setState({ orders: sorted });
-  //   }
-
-  //   if (sorting.user) {
-  //     const userCompare = (a, b) => {
-  //       if (a.first_name < b.first_name) {
-  //         return -1;
-  //       }
-  //       if (a.first_name > b.first_name) {
-  //         return 1;
-  //       }
-
-  //       if (a.last_name < b.last_name) {
-  //         return -1;
-  //       }
-  //       if (a.last_name > b.last_name) {
-  //         return 1;
-  //       }
-
-  //       return 0;
-  //     }
-
-  //     const correctOrders = orders.map(order => {
-  //       const {first_name, last_name} = getUserNameShort(order.user_id);
-  //       return { ...order, first_name, last_name };
-  //     });
-
-  //     const sorted = correctOrders.sort(userCompare);
-  //     const result = sorted.map(order => {
-  //       delete order.first_name;
-  //       delete order.last_name;
-  //       return order;
-  //     });
-  //     this.setState({ orders: result });
-  //   }
-  // }
 
   handleSort = (e) => {
     const map = {
@@ -229,8 +142,7 @@ export default class App extends React.Component {
       'User Info': 'user',
     };
     const { sorting, orders, users } = this.state;
-    // console.log(sorting);
-    const columnName = e.target.textContent;
+    const columnName = e.target.id;
     if (columnName !== 'Card Number') {
       Object.keys(sorting).forEach((key) => {
         // console.log(key);
@@ -238,16 +150,24 @@ export default class App extends React.Component {
       });
       const sorted = sortOrders(sorting, orders, users);
       this.setState({ sorting, orders: sorted });
-      
-      // this.sortOrders();
     }
   }
 
-  // e.target.innerHTML = `${e.target.innerHTML}<span>&#8595;</span>`;
-
-
   render() {
-    const { searchValue } = this.state;
+    const { searchValue, sorting } = this.state;
+    const map = {
+      'Order Amount': 'total',
+      'Order Date': 'created_at',
+      'Transaction ID': 'transaction_id',
+      'Card Type': 'card_type',
+      'Location' : 'location',
+      'User Info': 'user',
+    };
+    const getStortRow = condition => condition ? (<span>&#8595;</span>) : '';
+    // const row = () => (<span>&#8595;</span>);
+    // console.log(row(sorting.transaction_id));
+    console.log(sorting);
+
     return (
       <>
         <Table responsive bordered>
@@ -257,13 +177,13 @@ export default class App extends React.Component {
             <th colSpan="6"><input type="text" id="search" value={searchValue} onChange={this.handleChange}/></th>
           </tr>
             <tr onClick={this.handleSort}>
-              <th style={{cursor: "pointer"}}>Transaction ID</th>
-              <th style={{cursor: "pointer"}}>User Info</th>
-              <th style={{cursor: "pointer"}}>Order Date</th>
-              <th style={{cursor: "pointer"}} >Order Amount</th>
+              <th style={{cursor: "pointer"}} id='Transaction ID'>Transaction ID {getStortRow(sorting.transaction_id)}</th>
+              <th style={{cursor: "pointer"}} id='User Info'>User Info {getStortRow(sorting.user)}</th>
+              <th style={{cursor: "pointer"}} id='Order Date'>Order Date {getStortRow(sorting.created_at)}</th>
+              <th style={{cursor: "pointer"}} id='Order Amount'>Order Amount {getStortRow(sorting.total)}</th>
               <th>Card Number</th>
-              <th style={{cursor: "pointer"}}>Card Type</th>
-              <th style={{cursor: "pointer"}}>Location</th>
+              <th style={{cursor: "pointer"}} id='Card Type'>Card Type {getStortRow(sorting.card_type)}</th>
+              <th style={{cursor: "pointer"}} id='Location'>Location {getStortRow(sorting.location)}</th>
             </tr>
           </thead>
           <tbody>
